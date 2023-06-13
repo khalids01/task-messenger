b3e9480114f390f2da5875cd81acd12fdcb11193
khalids01 <khalids8774@gmail.com>
2023-06-13 13:11:18 +0600

    todos fetched from db

--- components/sections/board/Board.tsx
+++ components/sections/board/Board.tsx
@@ -1,7 +1,18 @@
+"use client";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-
+import getTodosGroupedByColumn from "@/lib/getTodosGroupedByColumn";
+import { useBoardStore } from "@/store/BoardStore";
 const Board = () => {
+  const { board, getBoard } = useBoardStore((state) => state);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  console.log(board);
+
   return (
     <section>
       {/* <DragDropContext>
